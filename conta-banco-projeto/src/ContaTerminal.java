093f82fda17f0ec6b1f2744d02aa76643a7509f8
lucasemanoel3103 <160548558+lucasemanoel3103@users.noreply.github.com>
2024-06-18 17:38:45 -0300

    exemplos de estruturas de repetições

--- conta-banco-projeto/src/ContaTerminal.java
+++ conta-banco-projeto/src/ContaTerminal.java
@@ -15,7 +15,7 @@ public class ContaTerminal {
         int numeroConta = scanner.nextInt();
 
         //imprimindo os dados obtidos pelo usuario
-        System.out.println("Olá, Lucas! sua Agencia é o itau, seu número de conta é 001 e seu saldo é 200,00R$");
+        System.out.println(" Olá, Lucas! " sua Agencia é o itau, seu número de conta é 001 e seu saldo é 200,00R$");
 
 
 
