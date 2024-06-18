1bfd5835a84beb0055c5e21045ebe32326ecf860
lucasemanoel3103 <160548558+lucasemanoel3103@users.noreply.github.com>
2024-06-17 22:09:52 -0300

    meu projeto conta banco finalizado

--- conta-banco-projeto/src/ContaTerminal.java
+++ conta-banco-projeto/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite a Agencia: ");
+        String Agencia = scanner.next();
+
+        System.out.println("Digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá, Lucas! sua Agencia é o itau, seu número de conta é 001 e seu saldo é 200,00R$");
+
+
+
+        
+
+
     }
 }
